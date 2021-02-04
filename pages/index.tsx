7a2e9e11a30195692eaf323256f97dbfdc2a3005
Douglas Moura <douglas.ademoura@gmail.com>
2021-02-03 21:36:48 -0300

    chore: executa o linter no componente

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
-import Head from 'next/head'
-import Template from '../components/template'
+import Head from 'next/head';
+import Template from '../components/template';
 
-const Home = () => {
-  return (
-    <Template>
-      <Head>
-        <title>Crianda aplicação com Next</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet" /> 
-      </Head>
-    </Template>
-  )
-}
+const Home = () => (
+  <Template>
+    <Head>
+      <title>Criando aplicação com Next</title>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet" />
+    </Head>
+  </Template>
+);
 
 export default Home;
